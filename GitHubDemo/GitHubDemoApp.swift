fb5967123b409825a39dffe9eb2552f53fc8a5e3
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-01-10 08:32:02 -0600

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Katelyn Gosnell on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
